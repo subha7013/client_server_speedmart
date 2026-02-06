b26e7ecb8b1e984140afaa91b442df06ebd2e887
Subhasish Nath <nathsubhasish55@gmail.com>
2026-02-06 21:18:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,33 @@
+// import express from "express";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import cors from "cors";
+
+// import adminAuthRoutes from "./routes/adminAuthRoutes.js";
+// import adminOrderRoutes from "./routes/adminOrderRoutes.js";
+
+// dotenv.config();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// /* ROUTES */
+// app.use("/api/admin", adminAuthRoutes);
+// app.use("/api/admin", adminOrderRoutes);
+
+// /* DB */
+// mongoose.connect(process.env.MONGO_URI)
+// .then(()=> console.log("MongoDB Connected"))
+// .catch(err=> console.log(err));
+
+// /* SERVER */
+// app.listen(process.env.PORT || 5000, ()=>{
+//  console.log("Server Running");
+// });
+
+
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -17,12 +47,19 @@ app.use(express.json());
 app.use("/api/admin", adminAuthRoutes);
 app.use("/api/admin", adminOrderRoutes);
 
-/* DB */
+/* DB CONNECT + SERVER START */
 mongoose.connect(process.env.MONGO_URI)
-.then(()=> console.log("MongoDB Connected"))
-.catch(err=> console.log(err));
+.then(()=>{
 
-/* SERVER */
-app.listen(process.env.PORT || 5000, ()=>{
- console.log("Server Running");
+ console.log("MongoDB Connected");
+
+ app.listen(process.env.PORT || 5000, ()=>{
+  console.log("Server Running");
+ });
+
+})
+.catch(err=>{
+ console.log("Mongo Error:", err);
+ process.exit(1);
 });
+
